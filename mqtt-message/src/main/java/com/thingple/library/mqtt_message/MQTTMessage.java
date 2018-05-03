632799e8633513abe00c5eb0aff5b2ab65c5aa87
lishimeng <alex.li@jxfinancial.cn>
2018-05-03 17:32:12 +0800

    u

---
+++ mqtt-message/src/main/java/com/thingple/library/mqtt_message/MQTTMessage.java
@@ -0,0 +1,19 @@
+package com.thingple.library.mqtt_message;
+
+/**
+ * 签到消息
+ * Created by lism on 2017/8/4.
+ */
+
+public class MQTTMessage {
+
+    /**
+     * topic
+     */
+    public String topic;
+
+    /**
+     * 消息内容
+     */
+    public String content;
+}
